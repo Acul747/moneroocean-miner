baf67e0c602ae361fd5cb6ad1ec425947391553f
Acul747 <73825497+Acul747@users.noreply.github.com>
2023-07-13 12:53:38 +0200

    Update limit_cpu_75.sh (#2)

--- limit_cpu_75.sh
+++ limit_cpu_75.sh
@@ -6,6 +6,13 @@ service_name="moneroocean_miner"
 # Define the CPU limit percentage
 cpu_limit=75
 
+# Check if cpulimit is installed
+if ! command -v cpulimit &> /dev/null; then
+    echo "cpulimit is not installed. Installing..."
+    sudo apt-get update
+    sudo apt-get install cpulimit -y
+fi
+
 # Get the PID of the service
 service_pid=$(systemctl show -p MainPID "$service_name" | awk -F= '{print $2}')
 
@@ -29,4 +36,5 @@ EOF
 sudo systemctl enable cpu_limit_after_reboot.service
 
 echo "CPU usage for service '$service_name' has been limited to $cpu_limit%."
-#echo "The CPU limit will be applied after every reboot as well."
+echo "The CPU limit will be applied after every reboot as well."
+
